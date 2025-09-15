674e449e95434848d167dcb26ab0c688f1ff6c8d
Ylen X Walker <verseoneplusone@gmail.com>
2025-09-15 19:30:34 +0800

    Revert "Update default config and limits"
    This reverts commit 8e284cd47f654f862db564dabf263f141abd2883.

--- src/common/mmo.hpp
+++ src/common/mmo.hpp
@@ -136,12 +136,9 @@ enum e_enchantgrade : uint16{
 	#define MAX_ARMOR_LEVEL 2
 	#define MAX_ENCHANTGRADE ENCHANTGRADE_A
 #else
-	//#define MAX_WEAPON_LEVEL 4
-	//#define MAX_ARMOR_LEVEL 1
-	//#define MAX_ENCHANTGRADE ENCHANTGRADE_NONE
-	#define MAX_WEAPON_LEVEL 5
-	#define MAX_ARMOR_LEVEL 2
-	#define MAX_ENCHANTGRADE ENCHANTGRADE_A
+	#define MAX_WEAPON_LEVEL 4
+	#define MAX_ARMOR_LEVEL 1
+	#define MAX_ENCHANTGRADE ENCHANTGRADE_NONE
 #endif
 
 // for produce
